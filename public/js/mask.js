920c3e2522c4300353730217b3b2106ac811b04f
Kazuhiro Homma <kazu.homma@gmail.com>
2014-06-07 14:45:03 +0900

    顔認識途中

---
+++ public/js/mask.js
@@ -0,0 +1,31 @@
+$(function(){
+  var videoInput = document.getElementById('their-video');
+  var canvasInput = document.getElementById('compare');
+  var canvasOverlay = document.getElementById('overlay')
+  var overlayContext = canvasOverlay.getContext('2d');
+
+  var htracker = new headtrackr.Tracker();
+  htracker.init(videoInput, canvasInput);
+  htracker.start();
+
+  var image = new Image();
+  setImage = function(index){
+    if (index) {
+      image.src = "/images/namie.png";
+    }
+  }
+  setImage(1);
+
+  document.addEventListener("facetrackingEvent", function( event ) {
+    overlayContext.clearRect(0 ,0, canvasOverlay.width, canvasOverlay.height);
+    if (event.detection == "CS") {
+      overlayContext.translate(event.x, event.y)
+      overlayContext.rotate(event.angle-(Math.PI/2));
+      overlayContext.drawImage(image,(-(event.height/2 + 25)) >> 0, (-(event.height/2 + 25)) >> 0, event.height + 50, event.height + 50);
+      overlayContext.rotate((Math.PI/2)-event.angle);
+      overlayContext.translate(-event.x, -event.y);
+    }
+  });
+
+});
+
